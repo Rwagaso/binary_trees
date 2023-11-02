8cd11e8275bad7094b660acb5583a8d077fbc7de
Rwagaso <stevenrwa31@gmail.com>
2023-11-02 11:29:13 -0700

    more

--- 123-avl_remove.c
+++ 123-avl_remove.c
@@ -1,135 +1,117 @@
+/* 30. Is AVL */
 #include "binary_trees.h"
+#include <limits.h>
+
+
+#ifndef MAX_INT
+#define MAX_INT
 
 /**
- * bal - Measures balance factor of a AVL
- * @tree: tree to go through
- * Return: balanced factor
+ * max_int - finds larger of two int values
+ * @a: first value to compare
+ * @b: second value to compare
+ * Return: larger int value, or value of both if equal
  */
-void bal(avl_t **tree)
+inline int max_int(int a, int b)
 {
-	int bval;
-
-	if (tree == NULL || *tree == NULL)
-		return;
-	if ((*tree)->left == NULL && (*tree)->right == NULL)
-		return;
-	bal(&(*tree)->left);
-	bal(&(*tree)->right);
-	bval = binary_tree_balance((const binary_tree_t *)*tree);
-	if (bval > 1)
-		*tree = binary_tree_rotate_right((binary_tree_t *)*tree);
-	else if (bval < -1)
-		*tree = binary_tree_rotate_left((binary_tree_t *)*tree);
+	return ((a > b) ? a : b);
 }
-/**
- * successor - get the next successor i mean the min node in the right subtree
- * @node: tree to check
- * Return: the min value of this tree
- */
-int successor(bst_t *node)
-{
-	int left = 0;
 
-	if (node == NULL)
+#endif
+
+
+/**
+ * BST_is_AVL_balanced - measures the balance factor of a binary tree
+ *
+ * @tree: root node from which to measure
+ * Return: height of `tree` left subtree minus height of right subtree,
+ * or 0 if `tree` is NULL
+ */
+avl_data_t BST_is_AVL_balanced(const binary_tree_t *tree)
+{
+	avl_data_t left, right, node;
+	int bal_factor;
+
+	if (!tree)
 	{
-		return (0);
+		node.height = -1;
+		node.is_AVL = 1;
+		return (node);
 	}
+
+	left = BST_is_AVL_balanced(tree->left);
+	right = BST_is_AVL_balanced(tree->right);
+
+	node.height = 1 + max_int(left.height, right.height);
+
+	bal_factor = right.height - left.height;
+	if (bal_factor < -1 || bal_factor > 1)
+		node.is_AVL = 0;
 	else
-	{
-		left = successor(node->left);
-		if (left == 0)
-		{
-			return (node->n);
-		}
-		return (left);
-	}
+		node.is_AVL = left.is_AVL && right.is_AVL;
 
+	return (node);
 }
-/**
- *remove_type - function that removes a node depending of its children
- *@root: node to remove
- *Return: 0 if it has no children or other value if it has
- */
-int remove_type(bst_t *root)
-{
-	int new_value = 0;
 
-	if (!root->left && !root->right)
+
+/**
+ * tree_is_BST - recursive helper to binary_tree_is_bst
+ * @tree: tree to check for BST
+ * @prev: pointer to int, passed "by reference" to update during recursion
+ * Return: 1 valid if `tree` is valid BST, or 0 if not or `tree` is NULL
+ */
+int tree_is_BST(const binary_tree_t *tree, int *prev)
+{
+	if (tree)
 	{
-		if (root->parent->right == root)
-			root->parent->right = NULL;
-		else
-			root->parent->left = NULL;
-		free(root);
+		/*
+		 * in-order traversal: recurse left, test, recurse right
+		 * branching left tests against prev inherited from parent
+		 * while branching right tests against prev = parent->n
+		 */
+		if (!tree_is_BST(tree->left, prev))
+			return (0);
+
+		/*
+		 * checks both for repeat values and for left_child < parent
+		 * and right_child > parent
+		 */
+		if (tree->n <= *prev)
+			return (0);
+
+		/* prev updates to current */
+		*prev = tree->n;
+
+		return (tree_is_BST(tree->right, prev));
+	}
+	/* recursion has reached an edge of the tree */
+	return (1);
+}
+
+
+/**
+ * binary_tree_is_avl - uses recursive helper to test first if binary tree
+ * is a Binary Search tree:
+ * - left subtree of each node only has values less than node
+ * - right subtree of each node only has values greater than node
+ * - tree is a BST at each node
+ * - no repeat values in this implementation
+ * then if tree is an AVL tree:
+ * - balance factor is between -1 and 1 at every node (no more than 1 level
+ * height difference between n->left and n->right)
+ *
+ * @tree: tree to check for AVL
+ * Return: 1 valid if `tree` is valid AVL, or 0 if not or `tree` is NULL
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	int prev = INT_MIN;
+
+	if (!tree)
 		return (0);
-	}
-	else if ((!root->left && root->right) || (!root->right && root->left))
-	{
-		if (!root->left)
-		{
-			if (root->parent->right == root)
-				root->parent->right = root->right;
-			else
-				root->parent->left = root->right;
-			root->right->parent = root->parent;
-		}
-		if (!root->right)
-		{
-			if (root->parent->right == root)
-				root->parent->right = root->left;
-			else
-				root->parent->left = root->left;
-			root->left->parent = root->parent;
-		}
-		free(root);
+
+	if (!tree_is_BST(tree, &prev))
 		return (0);
-	}
-	else
-	{
-		new_value = successor(root->right);
-		root->n = new_value;
-		return (new_value);
-	}
-}
-/**
- * bst_remove - remove a node from a BST tree
- * @root: root of the tree
- * @value: node with this value to remove
- * Return: the tree changed
- */
-bst_t *bst_remove(bst_t *root, int value)
-{
-	int type = 0;
 
-	if (root == NULL)
-		return (NULL);
-	if (value < root->n)
-		bst_remove(root->left, value);
-	else if (value > root->n)
-		bst_remove(root->right, value);
-	else if (value == root->n)
-	{
-		type = remove_type(root);
-		if (type != 0)
-			bst_remove(root->right, type);
-	}
-	else
-		return (NULL);
-	return (root);
-}
-
-/**
- * avl_remove - remove a node from a AVL tree
- * @root: root of the tree
- * @value: node with this value to remove
- * Return: the tree changed
- */
-avl_t *avl_remove(avl_t *root, int value)
-{
-	avl_t *root_a = (avl_t *) bst_remove((bst_t *) root, value);
-
-	if (root_a == NULL)
-		return (NULL);
-	bal(&root_a);
-	return (root_a);
+	return (BST_is_AVL_balanced(tree).is_AVL);
 }
